6033260e293cff981b1dd39cc3d8f2444202bac7
Dharani Reddy <dharani.reddy@emids.com>
2016-07-13 23:32:58 +0530

    Pods changed Dashboard beginning changes.

---
+++ Saint Joseph/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Saint Joseph
+//
+//  Created by Dharani on 7/13/16.
+//  Copyright © 2016 Dharani. All rights reserved.
+//
+
+import Foundation
+struct Constants {
+    
+    struct StoryBoard {
+        static let Main = "Main"
+    }
+    
+    struct NavigationController {
+        static let DashboardNavigationController = "DashboardNavigationController"
+    }
+}
--- Saint Joseph/MenuViewController.swift
+++ Saint Joseph/MenuViewController.swift
@@ -2,42 +2,18 @@
 //  MenuViewController.swift
 //  Saint Joseph
 //
-//  Created by Dharani on 7/4/16.
+//  Created by Dharani on 7/13/16.
 //  Copyright © 2016 Dharani. All rights reserved.
 //
 
 import UIKit
 
-class MenuViewController: ECSlidingViewController {
-    
-    override var topViewController: UIViewController! {
-        didSet {
-            topViewController.view.addGestureRecognizer(panGesture)
-            topViewAnchoredGesture = [.Panning, .Tapping]
-            
-            if let navigationController = topViewController as? UINavigationController {
-                if let 🍔 = navigationController.topViewController!.navigationItem.leftBarButtonItem {
-                    assert(🍔.tintColor == Colors.hamburgerTintColor(), "Storyboard hamburger tintColor should be set to \(Colors.hamburgerTintColor()) not \(🍔.tintColor)")
-                    🍔.target = self
-                    🍔.action = "hamburgerPressed:"
-                }
-            }
-        }
-    }
-    
+class MenuViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        IdleTimerManager.sharedInstance.scheduleNewIdleTimer()
-    }
-    
-    @objc private func hamburgerPressed(sender: UIBarButtonItem) {
-        if currentTopViewPosition == .Centered {
-            anchorTopViewToRightAnimated(true)
-        } else {
-            resetTopViewAnimated(true)
-        }
-    }
 
+        // Do any additional setup after loading the view.
+    }
 
 }
